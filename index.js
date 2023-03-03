a51a65b6adea70e3e880329beeb64b33697a1b45
Michace <michace@Michaces-MacBook-Pro.local>
2023-03-03 13:56:13 +0700

    add 4 code clinic from file excel - 03.03.2023

--- index.js
+++ index.js
@@ -32,155 +32,16 @@ app.post("/clinics/generates", async (req, res) => {
   }
 });
 
-app.get("/clinics/update", async (req, res) => {
-  try {
-    const listCodeNeedUpdated = [
-      "BVMED",
-      "BVTDHN",
-      "BVPSAC",
-      "BVQDMD",
-      "VINMEC",
-      "NKKIMSNA",
-      "YKVPCL",
-      "PKVTBH01",
-      "NKPDUONG",
-      "PKYKTMTC",
-      "NKMOT",
-      "PKBVDHYD",
-      "NKSTAR",
-      "NKNC",
-      "CAREPLUS",
-      "NKSDTP",
-      "MEKOMED",
-      "PKQTSGQ7",
-      "PKTDNT",
-    ];
-
-    let raw = fs.readFileSync("exports-clinics.json");
-    let data = JSON.parse(raw);
-
-    let tempArr = [];
-    listCodeNeedUpdated.map((code) => {
-      data.filter((item) => {
-        if (code === item.lisaCode) {
-          console.log(item);
-          tempArr.push(item);
-        }
-      });
-    });
-
-    res.send({
-      total: tempArr.length,
-      data: tempArr,
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-
 app.get("/clinics/add-new", async (req, res) => {
   try {
     const addNewClinicsListByLisaCode = [
-      "MEDICBD",
-      "BVSGPR",
-      "PK246TH",
-      "NKHAIAU",
-      "MEDIPTM",
-      "BVDATH",
-      "PKDKJH",
-      "NKBAQ7",
-      "BERNARD",
-      "MEDICBL",
-      "BVDKQTHL",
-      "MATSUOKA",
-      "BVANVIET",
-      "NKKIMQ12",
-      "NKKIMQ7",
+      "PKDKJHTB",
+      "PKDKJHTD",
+      "PKDKJHBD",
+      "BV175",
     ];
 
     let raw = fs.readFileSync("exports-clinics.json");
-    let data = JSON.parse(raw);
-
-    let tempArr = [];
-    addNewClinicsListByLisaCode.map((code) => {
-      data.filter((item) => {
-        if (code === item.lisaCode) {
-          console.log(item);
-          tempArr.push(item);
-        }
-      });
-    });
-
-    res.send({
-      total: tempArr.length,
-      data: tempArr,
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-
-app.get("/clinics/addition-22-clinics", async (req, res) => {
-  try {
-    const addNewClinicsListByLisaCode = [
-      "BVAS", // code from file July excel
-      "BVBNDTU", // code from file July excel
-      "CAHBD", // code from file July excel
-      "BVDHYHP", // code from file July excel
-      "BVBDDN", // code from file July excel
-      "BVDKDN", // code from file July excel
-      "BVGDDN", // code from file July excel
-      "BVDKHVT", // code from file July excel
-      "BVDKHMBN", // code from file July excel
-      "BVHMMH", // code from file July excel
-      "BVDKHN", // code from file July excel
-
-      "BVDHYD", // code from file December excel
-      "BVDHYD2", // code from file December excel
-      "BVHV", // code from file December excel
-      "RAFFLESM", // code from file December excel
-      "FMPHCM", // code from file December excel
-      "CARE1", // code from file December excel
-      "PKDKHS", // code from file December excel
-      "RAFFLESN", // code from file December excel
-      "FMPHN", // code from file December excel
-      "PKDKHSHN", // code from file December excel
-      "RAFLESVT", // code from file December excel
-    ];
-
-    let raw = fs.readFileSync("exports-clinics.json");
-    let data = JSON.parse(raw);
-
-    let tempArr = [];
-    addNewClinicsListByLisaCode.map((code) => {
-      data.filter((item) => {
-        if (code === item.lisaCode) {
-          console.log(item);
-          tempArr.push(item);
-        }
-      });
-    });
-
-    res.send({
-      total: tempArr.length,
-      data: tempArr,
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-
-app.get("/clinics/add-new-5-clinics", async (req, res) => {
-  try {
-    const addNewClinicsListByLisaCode = [
-      "PKHNTH",
-      "FVC",
-      "NKKIMQ6",
-      "NKKIMTDM",
-      "NKBAQ3",
-    ];
-
-    let raw = fs.readFileSync("exports-clinics-27122022.json");
     let data = JSON.parse(raw);
 
     let tempArr = [];
@@ -205,32 +66,6 @@ app.get("/clinics/add-new-5-clinics", async (req, res) => {
 app.get("/clinics/delete-2-clinics", async (req, res) => {
   try {
     const addNewClinicsListByLisaCode = ["VIETSING", "NKSO"];
-
-    let raw = fs.readFileSync("exports-clinics-27122022.json");
-    let data = JSON.parse(raw);
-
-    let tempArr = [];
-    addNewClinicsListByLisaCode.map((code) => {
-      data.filter((item) => {
-        if (code === item.lisaCode) {
-          console.log(item);
-          tempArr.push(item);
-        }
-      });
-    });
-
-    res.send({
-      total: tempArr.length,
-      data: tempArr,
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-
-app.get("/clinics/add-1-delete-6-clinics", async (req, res) => {
-  try {
-    const addNewClinicsListByLisaCode = ["BVDHYHN"];
 
     let raw = fs.readFileSync("exports-clinics-27122022.json");
     let data = JSON.parse(raw);
